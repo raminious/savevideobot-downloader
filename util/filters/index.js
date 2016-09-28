3177e08a0d041ac0ec84bce7f1d686578c0955d3
Ramin Mousavi <ramin.mousavy@gmail.com>
2016-09-28 19:02:45 +0330

    add dump filters

---
+++ util/filters/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = function () {
+  const filters = arguments
+  const fn = {}
+
+  for (let f in filters)
+    fn[filters[f]] = require('./' + filters[f])
+
+  return fn
+}
---
+++ util/filters/porn/index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const _ = require('underscore')
+const config = require('../../../config.json')
+const words = config.download.restrict.words
+
+module.exports = function (site, title, url) {
+
+  let find = false
+  const inputs = [site, title]
+
+  for (let i in inputs) {
+
+    // prepare input
+    let input = inputs[i].toLowerCase()
+
+    find = _.some(words, w => {
+      return input.match(w) != null
+    })
+
+    if (find) break
+  }
+
+  // log porn requests
+  if (find) {
+    throw { 
+      type: 'porn',
+      message: 'We are so sorry, You are not able to download pornographic contents.',
+      site,
+      title,
+      url
+    }
+  }
+}
