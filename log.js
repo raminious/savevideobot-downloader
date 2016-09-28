b0724e2bc14df65e6a5d2ddeddfc0556857cf8a0
Ramin Mousavi <ramin.mousavy@gmail.com>
2016-09-28 19:01:51 +0330

    make logger separate

---
+++ log.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const config = require('./config.json')
+const agent = require('superagent')
+
+module.exports = function (level, message, info) {
+  
+  agent
+  .post(config.log.url)
+  .auth(config.log.auth.username, config.log.auth.password, { type: 'auto' })
+  .send({
+    id: this.id,
+    level,
+    short_message: message,
+    from: 'downloader'
+  })
+  .send(info)
+  .end((err, res) => {})
+}
